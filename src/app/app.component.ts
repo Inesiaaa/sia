5714307ef97317e407c3035912349b1ca47e59bf
SEVY\HP <she.aglubat@gmail.com>
2025-09-25 06:25:22 +0800

    feature/portfolio-v1 - Created new components for portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsAndAbilitiesComponent } from './skills-and-abilities/skills-and-abilities.component';
+import { FeaturedProjectsComponent } from './featured-projects/featured-projects.component';
+import { FooterComponent } from './footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    HeaderComponent, 
+    BannerComponent,
+    AboutComponent,
+    SkillsAndAbilitiesComponent,
+    FeaturedProjectsComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
